3b62f78f3b626182c1fb3988552f3483581bacad
derrring <jiongyiwang@whu.edu.cn>
2020-07-12 09:23:09 +0800

    regular

--- python/project_alien_invaision/ship.py
+++ python/project_alien_invaision/ship.py
@@ -5,13 +5,13 @@ class Ship():
         self.screen = screen
 
 
-        self.image = pygame.image.load('.\\images\\ship.bmp')
+        self.image = pygame.image.load('./images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
 
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-def blitme(self):
-    #在指定位置绘制飞船
-    self.screen.blit(self.image,self.rect)
+    def blitme(self):
+        #在指定位置绘制飞船
+        self.screen.blit(self.image,self.rect)
