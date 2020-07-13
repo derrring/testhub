dc8af97f13f1b4299eac1ac58db3c929974bab46
derrring <jiongyiwang@whu.edu.cn>
2020-07-13 17:35:36 +0800

    game part 1

--- python/project_alien_invaision/alien_invaision.py
+++ python/project_alien_invaision/alien_invaision.py
@@ -17,9 +17,9 @@ def run_game():
 
 
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        bullets.update()
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- python/project_alien_invaision/game_functions.py
+++ python/project_alien_invaision/game_functions.py
@@ -3,35 +3,32 @@ import pygame
 from bullet import Bullet
 
 def check_keydown_events(event, ai_settings, screen, ship, bullets):
-    #响应按键
+    '''响应按键'''
     if event.key == pygame.K_RIGHT:
         ship.moving_right = True
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
         #创造一颗子弹，并将其加入到编组bullets中
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
+        fire_bullet(ai_settings, screen, ship, bullets)
 
-
-def check_keyup_events(event,ship):
-    #响应松开
+def check_keyup_events(event, ship):
+    '''响应松开'''
     if event.key == pygame.K_RIGHT:
         ship.moving_right = False
     elif event.key == pygame.K_LEFT:
         ship.moving_left = False
 
 def check_events(ai_settings, screen, ship, bullets):
-    for event in pygame.event.get(event, ai_settings, screen, ship, bullets):
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:            
-            check_keydown_events(shipship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event,ship)
 
 def update_screen(ai_settings, screen, ship, bullets):
-
     screen.fill(ai_settings.bg_color)
     #在飞船和外星人后面重绘所有的子弹
     for bullet in bullets.sprites():
@@ -39,3 +36,19 @@ def update_screen(ai_settings, screen, ship, bullets):
     ship.blitme()
 
     pygame.display.flip()
+
+def update_bullets(bullets):
+    '''更新子弹的位置，并删除已消失的子弹'''
+    #更新子弹的位置
+    bullets.update()
+    #删除已经消失的子弹
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    '''未达到全屏子弹上限时允许发射子弹'''
+    #创建新子弹，并将其加入bullets分组中
+    if len(bullets) <= ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
