3253d8f57fd38262eedfb34d3d6b82599862d246
derrring <jiongyiwang@whu.edu.cn>
2020-07-14 18:41:46 +0800

    reorganization

--- project_alien_invaision/ship.py
+++ project_alien_invaision/ship.py
@@ -4,11 +4,11 @@ class Ship():
     def __init__(self,ai_settings, screen):
         self.screen = screen
         self.ai_settings = ai_settings
-        #飞船样式
+        #set ship style
         self.image = pygame.image.load('./images/ship.bmp')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
-        #将每艘新飞船放在底部中央
+        #initialize position at the center of bottom
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
         #在飞船的属性center中储存小数值
